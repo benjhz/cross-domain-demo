b53427036189ec69c512e6a577318c722db72534
zhangjh <zhangjh@zhimei360.com>
2016-07-08 16:30:14 +0800

    add socket.io

--- server2/app.js
+++ server2/app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var serversConfig = require('./../config/servers');
 var server1 = serversConfig.server1;
 var server2 = serversConfig.server2;
@@ -12,16 +14,25 @@ app.use(express.static(__dirname + '/static'));
 app.get('/cors', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', server1Origin);
   res.setHeader('Access-Control-Allow-Credentials', true); //允许发送cookie
-  res.send('data from server2(CORS)');
+  res.send('data from server2 via CORS');
 });
 
 //方式二： JSONP
 app.get('/jsonp', function(req, res) {
   var callbackName = req.query.callback;
-  var resText = callbackName + '("' + 'data from server2(JSONP)' +'")';
+  var resText = callbackName + '("' + 'data from server2 via JSONP' +'")';
   res.send(resText);
 });
+console.log(io);
 
-app.listen(port, function() {
+//方式三： WebSocket
+io.on('connection', function(socket){
+  socket.on('data message', function(){
+    io.emit('data message', 'data from server2 via WebSocket');
+  });
+});
+
+
+http.listen(port, function() {
   console.log('server2 listening at http://localhost:%s', port);
 });
