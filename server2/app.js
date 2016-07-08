baa9973465e39389712c2fd4ada4f4de5ea6bbfc
zhangjh <zhangjh@zhimei360.com>
2016-07-08 15:09:01 +0800

    server1 get data from server2 via jsonp

--- server2/app.js
+++ server2/app.js
@@ -5,10 +5,18 @@ var server1Origin = "http://localhost:3000";
 
 app.use(express.static(__dirname + '/static'));
 
+//方式一： 跨域资源共享 (CORS)
 app.get('/foo', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', server1Origin);
   res.setHeader('Access-Control-Allow-Credentials', true); //允许发送cookie
-  res.send('data from server2');
+  res.send('data from server2(CORS)');
+});
+
+//方式二： JSONP
+app.get('/jsonp', function(req, res) {
+  var callbackName = req.query.callback;
+  var resText = callbackName + '("' + 'data from server2(JSONP)' +'")';
+  res.send(resText);
 });
 
 app.listen(port, function() {
